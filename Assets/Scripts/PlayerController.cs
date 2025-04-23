06a1a0923035b7a3bab6c3d55c77c62428105d8a
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-04-23 17:58:37 -0500

    Dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Horizontal Movement Settings")]
     [SerializeField] private float walkSpeed = 1;
+    [Space(5)]
 
     [Header("Vertical Movement Settings")]
     [SerializeField] private float jumpForce = 45;
@@ -15,17 +16,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float coyoteTime;
     private int airJumpCounter = 0;
     [SerializeField] private int maxAirJumps;
+    [Space(5)]
 
     [Header("Ground Check Settings")]
     [SerializeField] private Transform groundCheckPoint;
     [SerializeField] private float groundCheckY = 0.2f;
     [SerializeField] private float groundCheckX = 0.5f;
     [SerializeField] private LayerMask whatIsGround;
+    [Space(5)]
+
+    [Header("Dash Settings")]
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashTime = 0.2f;
+    [SerializeField] private float dashCooldown = 1f;
+    [Space(5)]
 
     PlayerStateList pState;
     private Rigidbody2D rb;
     private float xAxis;
+    private float gravity;
     Animator anim;
+    private bool canDash = true;
+    private bool dashed = false;
 
 
     public static PlayerController Instance;
@@ -52,6 +64,12 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         anim  = GetComponent<Animator>();
+
+        gravity = rb.gravityScale;
+
+        canDash = true; // Inicializado de nuevo para asegurar
+
+        dashed = false;
     }
 
     // Update is called once per frame
@@ -59,10 +77,12 @@ public class PlayerController : MonoBehaviour
     {
         GetInputs();
         UpdateJumpVariables();
+
+        if (pState.dashing) return;
         Flip();
         Move();
         Jump();
-
+        StartDash();
     }
 
     void GetInputs()
@@ -86,6 +106,35 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(xAxis * walkSpeed, rb.velocity.y);
         anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
+    }
+
+    void StartDash()
+    {
+        if (Grounded())
+        {
+            dashed = false;
+        }
+
+        if (Input.GetButtonDown("Dash") && canDash && !dashed)
+        {
+            StartCoroutine(Dash());
+            dashed = true;
+        }
+
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+        pState.dashing = true;
+        anim.SetTrigger("Dashing");
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = gravity;
+        pState.dashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 
     public bool Grounded()
