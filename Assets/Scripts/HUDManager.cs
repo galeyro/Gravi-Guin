162b0814b8edf6c4eaf1db352225b6ae74b7ac6e
Romelg18 <rggualoto10@gmail.com>
2025-05-28 16:59:55 -0500

    Implementado sistema de vida visual con HUD, daño y reinicio del jugador

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Image fillImage; // ← arrastrar aquí el Fill del Slider
+    private PlayerController player;
+
+    void Start()
+    {
+        player = PlayerController.Instance;
+        healthSlider.maxValue = player.maxHealth;
+        healthSlider.value = player.health;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float currentHealth = player.health;
+            float maxHealth = player.maxHealth;
+
+            healthSlider.value = currentHealth;
+
+            // Calcula el porcentaje
+            float healthPercent = currentHealth / maxHealth;
+
+            // Cambia el color según el porcentaje
+            if (healthPercent > 0.6f)
+            {
+                fillImage.color = Color.green;
+            }
+            else if (healthPercent > 0.3f)
+            {
+                fillImage.color = Color.yellow;
+            }
+            else
+            {
+                fillImage.color = Color.red;
+            }
+        }
+    }
+}
